aaba222749c29d9c5e22ffb206b1877bd1263c73
Putbotz <putbot604@gmail.com>
2022-08-27 17:13:15 +0800

    followersinstagram

---
+++ set.php
@@ -0,0 +1,15 @@
+<?php
+#MODDER: ADAM NOVICE
+#COUNTRY: INDONESIAN
+#DONT FORGET SUBSCRIBE
+#JANGAN RECODE SCRIPT TANPA IJIN, SALAM SUKSES🔥
+
+//masukan username akun tumbal
+$username_tumbal="xyl.4rk"; 
+
+//masukan password akun tumbal
+$sandi_tumbal="Putra604";
+
+//masukan username ig yang akan
+//di tambah follow
+$username="whooputt";
